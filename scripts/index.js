4e10d8d14b6972aa0a9d9c989ca0eb84d16a6711
Francisco García <francoffee57@gmail.com>
2022-02-11 18:15:04 -0600

    Agregué todos los sliders

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,25 @@ import { Slider } from "./Slider.mjs"
 const btnPlansLeft = document.getElementById('plansLeft')
 const btnPlansRight = document.getElementById('plansRight')
 
+const btnExchangeLeft = document.getElementById('exchangeLeft')
+const btnExchangeRight = document.getElementById('exchangeRight')
+
 const plansContainer = document.querySelector('.plans-card__container')
+const exchangeContainer = document.querySelector('.conteiner__tables-conteiner')
+
+
+const sliderExchange = new Slider(exchangeContainer, 0, 1, 100)
+
+btnExchangeLeft.addEventListener('click',()=>{
+    sliderExchange.move(-1)
+})
+
+btnExchangeRight.addEventListener('click',()=>{
+    sliderExchange.move(1)
+})
+
 
 const sliderPlans = new Slider(plansContainer, 1, 2, 110)
-
 
 btnPlansLeft.addEventListener('click',()=>{
     sliderPlans.move(-1)
@@ -15,3 +30,4 @@ btnPlansLeft.addEventListener('click',()=>{
 btnPlansRight.addEventListener('click',()=>{
     sliderPlans.move(1)
 })
+
